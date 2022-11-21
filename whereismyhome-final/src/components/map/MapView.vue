5903a942b604a7ad94a78d2a56ad625f02ff052a
윤소현 <wja06018@naver.com>
2022-11-21 10:41:54 +0900

    feat: Make KakaoMap

---
+++ whereismyhome-final/src/components/map/MapView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width: 100%; height: 100%">
+    <div id="map" style="width: 100%; height: 100%"></div>
+  </div>
+</template>
+
+<script>
+import { addScript, initMap } from "@/api/lib/kakaomap.js";
+
+export default {
+  name: "MapView",
+
+  data() {
+    return {
+      map: null,
+      markers: [],
+      latitude: 0,
+      longitude: 0,
+    };
+  },
+
+  mounted() {
+    window.kakao && window.kakao.maps ? initMap() : addScript();
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
