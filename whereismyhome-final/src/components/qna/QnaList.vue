d4ee78924d5dc9764c9632a41572dfe507262177
윤소현 <wja06018@naver.com>
2022-11-22 02:18:12 +0900

    feat: Qna UI - ans & list

--- whereismyhome-final/src/components/qna/QnaList.vue
+++ whereismyhome-final/src/components/qna/QnaList.vue
@@ -1,19 +1,114 @@
 <template>
-  <div>qnalist</div>
+  <div class="container">
+    <div class="list">
+      <ul>
+        <li class="head">
+          <div class="th1">작성일</div>
+          <div class="th2">제목</div>
+          <div class="th3">답변여부</div>
+        </li>
+        <li>
+          <qna-list-item
+            v-for="qna in qnas"
+            :key="qna.questionNo"
+            v-bind="qna"
+          ></qna-list-item>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+// import { mapActions, mapGetters, mapState } from "vuex";
+import QnaListItem from "./item/QnaListItem.vue";
+
+// const noticeStore = "noticeStore";
+// const memberStore = "memberStore";
+
 export default {
   name: "QnaList",
 
   data() {
-    return {};
+    return {
+      qnas: [
+        {
+          questionNo: "1",
+          userId: "T",
+          subject: "문의드려요",
+          content: "문의내용~~",
+          answer: "답답답답",
+          registerTime: "2022-11-12 21:16:16",
+        },
+        {
+          questionNo: "2",
+          userId: "T",
+          subject: "문의드려요22",
+          content: "문의내용~~",
+          answer: null,
+          registerTime: "2022-11-13 21:16:16",
+        },
+      ],
+    };
   },
 
-  mounted() {},
+  components: {
+    QnaListItem,
+  },
 
-  methods: {},
+  computed: {
+    // ...mapState(noticeStore, ["notices"]),
+  },
+
+  created() {
+    // this.getNoticesInfo();
+  },
+
+  methods: {
+    // ...mapActions(noticeStore, ["getNotices"]),
+    // async getNoticesInfo() {
+    //   await this.getNotices();
+    // },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.list {
+  width: 60%;
+  margin: auto;
+}
+
+ul {
+  margin-top: 50px;
+  width: 100%;
+  text-align: start;
+  border-collapse: collapse;
+  font-size: 16px;
+  table-layout: fixed;
+}
+
+.head {
+  display: flex;
+  border-top: 1px solid rgb(34, 34, 34);
+  border-bottom: 1px solid rgb(204, 204, 204);
+}
+
+.head div {
+  text-align: start;
+  padding: 19px 0px 17px 0px;
+  font-weight: 900;
+}
+
+.th1 {
+  width: 15%;
+}
+
+.th2 {
+  width: 75%;
+}
+
+.th3 {
+  text-align: center;
+}
+</style>
---
+++ whereismyhome-final/src/components/qna/item/QnaListItem.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="container">
+    <div class="head" @click="toggle">
+      <div class="th1">{{ this.registerTime.split(" ")[0] }}</div>
+      <div class="th2">{{ subject }}</div>
+      <div class="th3">{{ answer === null ? "답변예정" : "답변완료" }}</div>
+    </div>
+    <div class="answer" v-if="isActive">
+      <h4>문의내용</h4>
+      <div>
+        {{ content }}
+        <div class="button-wrapper">
+          <button @click="modifyQuestion">수정</button> |
+          <button @click="deleteQuestion">삭제</button>
+        </div>
+      </div>
+      <div class="answer-wrapper" v-if="answer !== null">
+        <h4>문의답변</h4>
+        <div>
+          {{ answer }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QnaListItem",
+
+  props: {
+    registerTime: String,
+    questionNo: String,
+    userId: String,
+    subject: String,
+    content: String,
+    answer: String,
+  },
+
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive;
+    },
+    modifyQuestion() {
+      console.log("수정해야해");
+    },
+    deleteQuestion() {
+      console.log("삭제해야해");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.head {
+  display: flex;
+  border-bottom: 1px solid rgb(204, 204, 204);
+  cursor: pointer;
+  width: 100%;
+}
+
+.head div {
+  text-align: start;
+  padding: 19px 0px 17px 0px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.th1 {
+  width: 15%;
+}
+
+.th2 {
+  width: 75%;
+}
+
+.th3 {
+  text-align: center;
+}
+
+.head:hover {
+  background-color: rgb(251, 251, 251);
+}
+
+.answer {
+  padding: 40px 50px;
+  border-bottom: 1px solid rgb(238, 238, 238);
+  background-color: rgb(250, 250, 250);
+}
+
+h4 {
+  margin: 0;
+}
+
+.button-wrapper {
+  margin-top: 2px;
+  float: right;
+}
+
+.button-wrapper button {
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  color: rgb(0, 21, 255);
+  cursor: pointer;
+}
+
+.answer-wrapper {
+  margin-top: 20px;
+}
+</style>
